deab4956a07b7c6f0a1a3f2e9a5fae5b4c08b07e
SafaaFathii <safaafathii123@gmail.com>
2022-05-19 17:43:02 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Given a set of features extracted from the shape pf the beans in images. it's required to predict the type of each bean.',
+    author='Safaa Fathii',
+    license='',
+)
